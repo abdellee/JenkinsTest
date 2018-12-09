924b8fa8bd7305aa80caaad2749d19f23f6f7601
Magnalane Coders <shamroukh62@hotmail.com>
2018-12-08 23:16:24 -0600

    Added Jenkins test project initially

--- JenkinsTest/Logger.cs
+++ JenkinsTest/Logger.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace JenkinsTest
 {
-    public class Class1
+    public class Logger
     {
+        public static void log(string msg)
+        {
+            // do something
+        }
     }
 }
